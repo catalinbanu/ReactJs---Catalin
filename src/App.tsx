f69667871036aca71489840a9cbd2fb1a2035b22
WHQ_NT_DOMAIN\CB083492 <cata_banu98@yahoo.com>
2020-08-19 13:07:29 +0300

    Import my library in app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ExampleComponent } from "cxb-library";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <ExampleComponent></ExampleComponent>
+    </div>
+  );
+}
+
+export default App;
